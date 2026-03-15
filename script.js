a4ec444f6f438ccbc90754a78e4384ebdf0c6e31
KAVYA VIRENDRA SHAH <232231600+shahkavya10@users.noreply.github.com>
2026-03-15 13:24:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -127,3 +127,13 @@ onhover: { enable: true, mode: "repulse" }
 });
 
 });
+
+
+/* MOBILE MENU */
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+navLinks.classList.toggle("active");
+});
